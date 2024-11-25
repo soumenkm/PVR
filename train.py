09ed8c901d068574fd2b4334d75dbf8b0063430b
Soumen Mondal <mondalsoumen00@gmail.com>
2024-11-25 22:19:34 +0000

    Add train.py

--- train.py
+++ train.py
@@ -1,13 +1,11 @@
-import os, json, pickle, torch, argparse
-torch.manual_seed(42)
+import os, json, pickle, torch, argparse, tqdm
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 from pathlib import Path
+from torch.utils.data import DataLoader
 import wandb
-# wandb.login()
-from transformers import TrainingArguments, Trainer, DefaultDataCollator, get_linear_schedule_with_warmup
 from dataset import LogicDataset
 from model import PVRModel
 import numpy as np
-import evaluate
 from typing import List, Tuple, Union, Any
 
 class PVRTrainer:
@@ -15,159 +13,129 @@ class PVRTrainer:
         self.config = config
         self.device = device
         self.model_name = self.config["model_name"]
-        self.model_name_srt = self.model_name.split("/")[-1]
+        
+        self.train_ds = LogicDataset(filepath=self.config["train_dataset_path"], model_name=self.model_name, frac=self.config["train_frac"])
+        self.test_ds = LogicDataset(filepath=self.config["test_dataset_path"], model_name=self.model_name, frac=self.config["test_frac"])
 
-        self.train_ds = LogicDataset(filepath=self.config["train_dataset_path"], model_name=self.config["model_name"], frac=self.config["train_frac"])
-        self.eval_ds = LogicDataset(filepath=self.config["test_dataset_path"], model_name=self.config["model_name"], frac=self.config["eval_frac"])
-        self.data_collator = DefaultDataCollator(return_tensors="pt")
+        self.train_dataloader = DataLoader(self.train_ds, batch_size=self.config["batch_size"], shuffle=True, num_workers=4)
+        self.test_dataloader = DataLoader(self.train_ds, batch_size=self.config["batch_size"], shuffle=True, num_workers=4)
 
-        self.batch_size = self.config["batch_size"]
-        self.config["num_steps"] = len(self.train_ds)//self.batch_size
-        self.num_steps = self.config["num_steps"]
         self.num_epochs = self.config["num_epochs"]
-        self.project_name = f"{self.model_name_srt}_train"
+        self.project_name = "PVR_train"
         os.environ["WANDB_PROJECT"] = self.project_name
         
-        self.run_name = f"{self.config['initial_lr']:.1e}"
-        self.model = PVRModel(d_model=1024, model_name=self.config["model_name"])
-            
-        self.output_dir = f"outputs/ckpt/{self.project_name}/{self.run_name}"
+        self.run_name = f"{self.config['train_frac']:.2f}_{self.config['initial_lr']:.1e}"
+        self.model = PVRModel(d_model=1024, model_name=self.model_name).to(self.device)
         self.optimizer = torch.optim.AdamW(params=self.model.parameters(), lr=self.config['initial_lr'], weight_decay=self.config["weight_decay"], betas=self.config["adam_betas"])
-        self.scheduler = get_linear_schedule_with_warmup(optimizer=self.optimizer,
-                                                         num_warmup_steps=int(0.01 * self.num_steps), 
-                                                         num_training_steps=int(self.num_epochs * self.num_steps))
+   
+        self.output_dir = Path(Path.cwd(), f"outputs/ckpt/{self.project_name}/{self.run_name}")
+        if not self.output_dir.exists():
+            self.output_dir.mkdir(parents=True, exist_ok=True)
+    
+    def _save_checkpoint(self, epoch: int) -> None:
+        checkpoint_path = Path(self.output_dir, f"model_epoch_{epoch}.pt")
+        data = {"state_dict": self.model.state_dict(), "config": self.config}
+        torch.save(data, checkpoint_path)
+        print(f"Model checkpoint saved at {checkpoint_path}")
+    
+    def _evaluate(self, epoch: int) -> None:
+        epoch_loss = 0.0
+        epoch_acc = 0.0
+        with tqdm.tqdm(self.test_dataloader, desc=f"Eval Epoch [{epoch}/{self.num_epochs}]") as pbar:
+            for batch_idx, batch in enumerate(pbar):
+                batch = {key: val.to(self.device) for key, val in batch.items()}
+                out = self.model(question_ids=batch["question"], rules_ids=batch["rules"], facts_ids=batch["facts"], labels=batch["answer"])
+                logits = out["logits"]
+                loss = out["loss"]
+                
+                epoch_loss += loss.item()
+                acc = (logits.argmax(dim=1) == batch["answer"]).to(float).mean()
+                epoch_acc += acc.item()
+                
+                pbar.set_postfix({"Batch Loss": f"{loss.item():.4f}",
+                                    "Batch Acc": f"{acc.item():.4f}"})
+                if self.config["wandb_log"]:
+                    wandb.log({"Eval Loss": loss.item(), "Eval Acc": acc.item(), "Epoch": epoch, "Step": self.num_eval_steps})
+                    self.num_eval_steps += 1
 
-        self.train_arg_config = {
-            "output_dir": self.output_dir,
-            "eval_strategy": "steps",
-            "eval_steps": max(self.batch_size, self.num_steps//self.config["num_ckpt_per_epoch"]),
-            "per_device_train_batch_size": self.batch_size,
-            "per_device_eval_batch_size": self.batch_size,
-            "gradient_accumulation_steps": self.config["grad_acc_steps"],
-            "max_grad_norm": self.config["max_grad_norm"],
-            "num_train_epochs": self.num_epochs,
-            "logging_strategy": "steps",
-            "logging_first_step": True,
-            "logging_steps": self.batch_size,
-            "save_strategy": "steps",
-            "save_steps": max(self.batch_size, self.num_steps//self.config["num_ckpt_per_epoch"]),
-            "save_safetensors": False, 
-            "save_total_limit": self.num_epochs * self.config["num_ckpt_per_epoch"] + 1,
-            "save_only_model": False,
-            "fp16": False,
-            "bf16": False,
-            "dataloader_drop_last": True,
-            "run_name": self.run_name,
-            "report_to": "wandb" if self.config["wandb_log"] else "none",
-            "eval_on_start": False
-        }
-
-        self.training_args = TrainingArguments(**self.train_arg_config)
-        self.trainer_config = {
-            "model": self.model,
-            "args": self.training_args,
-            "data_collator": self.data_collator,
-            "train_dataset": self.train_ds,
-            "eval_dataset": self.eval_ds,
-            "optimizers": (self.optimizer, self.scheduler),
-            "compute_metrics": self.compute_metrics
-        }
-        self.trainer = Trainer(**self.trainer_config)
-    
-    @staticmethod
-    def compute_metrics(eval_pred: Tuple[np.ndarray, np.ndarray]) -> dict:
-        predictions, labels = eval_pred
-        min_len = min(len(predictions), len(labels))
-        predictions = np.argmax(predictions[:min_len], axis=1)
-        labels = labels[:min_len]
-        accuracy = evaluate.load("accuracy")
-        output = accuracy.compute(predictions=predictions, references=labels) # keys: {"accuracy"}
-        return output
-    
-    @staticmethod
-    def compute_loss(self, model: torch.nn.Module, inputs: dict, outputs: torch.tensor = None):
-        """Not to be used by LoRAFineTuner.
-        Go to: ~/miniconda3/envs/env_name/lib/python3.8/site-packages/transformers/trainer.py
-        Modify Trainer.compute_loss() function.
-        Add this code snippet just before the return statement
-        """
-        # Custom: Begin.
-        if model.training:
-            total_norm = 0.0
-            for p in model.parameters():
-                if p.requires_grad:
-                    param_norm = p.norm(2).item()
-                    total_norm += param_norm ** 2
-            total_norm = total_norm ** 0.5
-
-            preds = outputs["logits"].detach() # outputs is already calculate before
-            acc = (preds.argmax(axis=1) == inputs["labels"]).to(torch.float).mean().item()
-            if self.state.global_step % self.args.logging_steps == 0:
-                self.log({"accuracy": acc, "param_norm": total_norm})
-        # Custom: End.
-    
-    def _save_config(self) -> None: 
-        config_data = {
-            "config": self.config,
-            "output_dir": self.output_dir,
-            "project_name": self.project_name,
-            "run_name": self.run_name,
-            "train_arg_config": self.train_arg_config
-        }
-        with open(self.output_dir + "/master_config.pkl", 'wb') as f:
-            pickle.dump(config_data, f)
-    
-    # @staticmethod
-    # def load_model(device: torch.device, config_path: Path, checkpoint_name: str) -> dict:
-    #     with open(config_path, 'rb') as f:
-    #         config_data = pickle.load(f)
-    #     config = config_data["config"]
-        
-    #     if config["finetune_type"] == "lora":     
-    #         model = VisionModelForCLSWithLoRA(device=device, model_name=config["model_name"], num_classes=config["num_classes"], lora_rank=config["lora_rank"], lora_alpha=config["lora_alpha"], linear_names=config["lora_linear_names"])
-    #     elif config["finetune_type"] == "layer":
-    #         model = VisionModelForCLS(device=device, model_name=config["model_name"], num_classes=config["num_classes"])
-    #     else:
-    #         raise ValueError("Invalid finetune type!")
-        
-    #     checkpoint_path = Path(config_path.parent, checkpoint_name)
-    #     checkpoint = torch.load(checkpoint_path, map_location=device)
-    #     model.load_state_dict(checkpoint, strict=False)
-    #     print(f"Model loaded from checkpoint: {checkpoint_path}")
-    #     return {"model": model, "config_data": config_data}
+        avg_loss = epoch_loss / len(self.test_dataloader)
+        avg_acc = epoch_acc / len(self.test_dataloader)
+        print(f"Eval Epoch [{epoch}/{self.num_epochs}] completed with Average Loss: {avg_loss:.4f}, Acc: {avg_acc:.4f}")
     
     def train(self) -> None:
-        self._save_config()
-        print(self.model)
-        print(self.model.calc_num_params())
-        self.trainer.train(resume_from_checkpoint=False)
-    
+        self.model.calc_num_params()
+        if self.config["wandb_log"]:
+            wandb.init(project=self.project_name, name=self.run_name, config=self.config)
+            self.num_eval_steps = 0
+            self.num_steps = 0
+        for epoch in range(1, self.num_epochs + 1):
+            epoch_loss = 0.0
+            epoch_acc = 0.0
+            with tqdm.tqdm(self.train_dataloader, desc=f"Train Epoch [{epoch}/{self.num_epochs}]") as pbar:
+                for batch_idx, batch in enumerate(pbar):
+                    batch = {key: val.to(self.device) for key, val in batch.items()}
+                    out = self.model(question_ids=batch["question"], rules_ids=batch["rules"], facts_ids=batch["facts"], labels=batch["answer"])
+                    logits = out["logits"]
+                    loss = out["loss"]
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                    
+                    epoch_loss += loss.item()
+                    acc = (logits.argmax(dim=1) == batch["answer"]).to(float).mean()
+                    epoch_acc += acc.item()
+                    
+                    pbar.set_postfix({"Batch Loss": f"{loss.item():.4f}",
+                                      "Batch Acc": f"{acc.item():.4f}"})
+                    if self.config["wandb_log"]:
+                        wandb.log({"Train Loss": loss.item(), "Train Acc": acc.item(), "Epoch": epoch, "Step": self.num_steps})
+                        self.num_steps += 1
+
+            if epoch % 1 == 0:
+                self._save_checkpoint(epoch=epoch)
+                self._evaluate(epoch=epoch)
+            
+            avg_loss = epoch_loss / len(self.train_dataloader)
+            avg_acc = epoch_acc / len(self.train_dataloader)
+            print(f"Train Epoch [{epoch}/{self.num_epochs}] completed with Average Loss: {avg_loss:.4f}, Acc: {avg_acc:.4f}")
+        
+        if self.config["wandb_log"]:
+            wandb.finish()
+
+    @staticmethod
+    def load_model(device: torch.device, checkpoint_path: Path) -> dict:
+        ckpt = torch.load(checkpoint_path, map_location=device)
+        config = ckpt["config"]
+        model = PVRModel(d_model=1024, model_name=config["model_name"])
+        model.load_state_dict(ckpt["state_dict"], strict=False)
+        print(f"Model loaded from checkpoint: {checkpoint_path}")
+        return {"model": model, "config_data": config}
+            
 def main(device: torch.device) -> None:
     config = {
         "model_name": "LIAMF-USP/roberta-large-finetuned-race",
         "train_dataset_path": Path(Path.cwd(), "data/depth-5/meta-train.jsonl"),
         "test_dataset_path": Path(Path.cwd(), "data/depth-5/meta-test.jsonl"),
-        "train_frac": 0.01,
-        "eval_frac": 0.01,
-        "num_epochs": 10,
+        "train_frac": 1.0,
+        "test_frac": 0.1,
+        "num_epochs": 5,
         "num_steps": None,
         "batch_size": 16,
-        "initial_lr": 5e-5,
-        "max_grad_norm": 10.0,
+        "initial_lr": 1e-5,
+        "max_grad_norm": 1.0,
         "weight_decay": 0.1,
         "adam_betas": (0.95, 0.999),
         "grad_acc_steps": 1,
         "num_ckpt_per_epoch": 1,
-        "wandb_log": False
+        "wandb_log": True
     }
 
     trainer = PVRTrainer(device=device, config=config)
     trainer.train()
     
-    # out = FineTuner.load_model(
+    # out = PVRTrainer.load_model(
     #     device="cuda", 
-    #     config_path=Path(Path.cwd(), "Project/outputs/ckpt/dinov2-base_finetune/layer_0.10_1.0e-05/master_config.pkl"),
-    #     checkpoint_name="checkpoint-100/pytorch_model.bin"
+    #     checkpoint_path=Path(Path.cwd(), "outputs/ckpt/PVR_train/0.01_5.0e-05/model_epoch_1.pt"),
     # )
     # print(out)
 
